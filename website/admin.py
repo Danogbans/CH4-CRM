94c71cc4f03dc18d60d54c030c95570ef5fcdcd9
Daniel Ogbansiegbe <danogbans@gmail.com>
2023-08-02 16:44:02 +0200

    Update admin.py

--- website/admin.py
+++ website/admin.py
@@ -2,5 +2,5 @@ from django.contrib import admin
 from .models import Record
 
 
-# -------------------------------- Models Registration -----------------------------------
+# -------------------------------- Models Registration ---------------------------------
 admin.site.register(Record)
