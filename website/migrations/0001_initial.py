81fab52bc92f7f84f99e2c1c7dbac841cb4db399
Daniel Ogbansiegbe <danogbans@gmail.com>
2024-08-19 23:23:43 +0200

    Update 0001_initial.py

--- website/migrations/0001_initial.py
+++ website/migrations/0001_initial.py
@@ -15,7 +15,7 @@ class Migration(migrations.Migration):
             name='Record',
             fields=[
                 ('id', models.BigAutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')), 
-                ('creation_date', models.DateTimeField(auto_now_add=True)),
+                ('creation_date', models.DateTimeField(auto_now_add=True)), 
                 ('first_name', models.CharField(max_length=50)),
                 ('last_name', models.CharField(max_length=50)),
                 ('email', models.CharField(max_length=100)),
