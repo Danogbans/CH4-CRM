c319eb4cfc0de466b03951175623d4d5b2df9092
Daniel Ogbansiegbe <danogbans@gmail.com>
2024-08-31 21:58:55 +0200

    Update settings.py

--- dcrm/settings.py
+++ dcrm/settings.py
@@ -4,7 +4,7 @@ from pathlib import Path
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 
-# Quick-start development settings - unsuitable for 
+# Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
